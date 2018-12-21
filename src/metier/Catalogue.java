78fd803b2715a5d8bc1e1d71e759b80c591a6b2d
AxelCom* <axel.commergnat@hotmail.com>
2018-12-21 19:11:52 +0100

    Re-Initialisation Projet with 4 test non fonctionnel

---
+++ src/metier/Catalogue.java
@@ -0,0 +1,172 @@
+package metier;
+
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import metier.*;
+public class Catalogue implements I_Catalogue {
+	
+	private ArrayList<I_Produit> lesProduits = new ArrayList<I_Produit>();
+	
+	public Catalogue() {
+		
+	}
+	
+	@Override //DONE
+	public boolean addProduit(I_Produit produit) {
+		try {
+			if(!nameAlreadyUse(produit.getNom()) && produit.getPrixUnitaireHT() > 0 && produit.getQuantite() >= 0 ) {
+			lesProduits.add(produit);
+			return true;
+			}
+			return false;
+		}
+		catch(NullPointerException e) {
+			return false;
+		}
+	}
+
+	@Override //DONE
+	public boolean addProduit(String nom, double prix, int qte) {
+		try {
+			if(!nameAlreadyUse(nom)  && prix > 0 && qte >= 0) {
+				Produit leProduit = new Produit(nom,prix,qte);
+				lesProduits.add(leProduit);
+				return true;
+			}
+			return false;
+		}
+		catch(NullPointerException e){
+			return false;
+		}
+
+	}
+
+	@Override //DONE???
+	public int addProduits(List<I_Produit> l) {
+		try {
+			int nbInsert = 0;
+		for (I_Produit leProduit : l) {
+			if(leProduit.getPrixUnitaireHT() > 0 && leProduit.getQuantite() >= 0 && !nameAlreadyUse(leProduit.getNom())) {
+				lesProduits.add(leProduit);
+				nbInsert++;
+			}
+		}
+		return nbInsert;
+		}
+		catch(NullPointerException e){
+			return 0;
+		}
+	}
+
+	@Override // DONE ??? check flo
+	public boolean removeProduit(String nom) {
+		int i = 0;
+		
+		while(i<(lesProduits.size()-1) && !lesProduits.get(i).getNom().equals(nom))
+		{
+			i++;
+		}
+		if(lesProduits.get(i).getNom().equals(nom))
+		{
+			lesProduits.remove(lesProduits.get(i));
+			return true;
+		}
+		return false;
+	}
+
+	@Override // DONE ?? check flo
+	public boolean acheterStock(String nomProduit, int qteAchetee) {
+		int i = 0;
+		if(qteAchetee > 0) {
+			while(i<(lesProduits.size()-1) && !lesProduits.get(i).getNom().equals(nomProduit))
+			{
+				i++;
+			}
+			if(lesProduits.get(i).getNom().equals(nomProduit))
+			{
+				lesProduits.get(i).ajouter(qteAchetee);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override // DONE ?? check flo
+	public boolean vendreStock(String nomProduit, int qteVendue) {
+			int i = 0;
+			if(qteVendue > 0) {
+				while(i<(lesProduits.size()-1) && !lesProduits.get(i).getNom().equals(nomProduit))
+				{
+					i++;
+				}
+				if(lesProduits.get(i).getNom().equals(nomProduit))
+				{
+					if(lesProduits.get(i).getQuantite() >= qteVendue ) {
+						lesProduits.get(i).enlever(qteVendue);
+						return true;
+					}
+				}
+				return false;
+			}
+			return false;
+	}
+
+	@Override //DONE ??? //TODO STRING[] -> ARRAYLIST.sort -> STRING[]
+	public String[] getNomProduits() {
+		int count = 0;
+		String [] lesNoms = new String[lesProduits.size()];
+		for (I_Produit leProduit : this.lesProduits) {
+			lesNoms[count] = leProduit.getNom();
+			count++;
+		}
+		return lesNoms;
+	}
+
+	@Override //DONE ???
+	public double getMontantTotalTTC() {
+		double result = 0;
+		for (I_Produit leProduit : lesProduits) {
+			result += leProduit.getPrixStockTTC();
+		}
+		//DecimalFormat df2 = new DecimalFormat("#0.00");
+		//String test = df2.format(result);
+		//double result1 = Double.parseDouble(test);
+		//System.out.println(test);
+		return Math.round(result*100.0)/100.0;
+	}
+
+	@Override //DONE
+	public void clear() {
+		this.lesProduits.clear();
+	}
+	private boolean nameAlreadyUse(String nom) {
+		for (String unNom : getNomProduits()) {
+			nom = nom.trim();
+			if(nom.equals(unNom)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		if(lesProduits.size() > 0) {
+			//TODO
+			String phrases = "";
+			for (I_Produit unProd : lesProduits) {
+				phrases += unProd;
+			}
+			DecimalFormat df2 = new DecimalFormat("#0.00");
+			String montantTTTC = df2.format(this.getMontantTotalTTC());
+			return  phrases +"\n" +"Montant total TTC du stock : "+ montantTTTC +" €";
+		}
+		else {					 
+			return  "\n" +"Montant total TTC du stock : 0,00 €";
+		}
+	}
+}
+
---
+++ src/metier/Produit.java
@@ -0,0 +1,67 @@
+package metier;
+
+import java.text.DecimalFormat;
+
+public class Produit implements I_Produit {
+
+	private int qteStock;
+	private String nomProduit;
+	private double prixUnitaireHT;
+	private static double tauxTVA = 0.2F;
+	
+	public Produit(String leNom, double lePrix, int laQuantite) {
+		leNom.replaceAll("\t", " ");
+		this.nomProduit = leNom.trim();
+		this.prixUnitaireHT = lePrix;
+		this.qteStock = laQuantite;
+	}
+	
+	@Override//DONE
+	public boolean ajouter(int qteAchetee) {
+		//check negatif ??
+		this.qteStock += qteAchetee;
+		return true;
+	}
+
+	@Override//DONE
+	public boolean enlever(int qteVendue) {
+		//check negatif ??
+		this.qteStock -= qteVendue;
+		return true;
+	}
+
+	@Override // DONE
+	public String getNom() {
+		return this.nomProduit;
+	}
+
+	@Override // DONE
+	public int getQuantite() {
+		return this.qteStock;
+	}
+
+	@Override // DONE
+	public double getPrixUnitaireHT() {
+		return this.prixUnitaireHT;
+	}
+
+	@Override // DONE
+	public double getPrixUnitaireTTC() {
+		return this.prixUnitaireHT+(this.prixUnitaireHT*tauxTVA);
+	}
+
+	@Override // DONE
+	public double getPrixStockTTC() {
+		return this.qteStock*getPrixUnitaireTTC();
+	}
+	
+	@Override
+	public String toString() {
+		DecimalFormat df2 = new DecimalFormat("#0.00");
+		 String prixU = df2.format(prixUnitaireHT);
+		 String prixTTC = df2.format(getPrixUnitaireTTC());
+		return this.nomProduit + " - prix HT : " + prixU + " € - prix TTC : "+ prixTTC +" € - quantité en stock : "+ this.qteStock + "\n";
+	}
+
+}
+
